77b77196f394443eebf42f287c9e3b55405048a7
austinmartinn <mugeshkumaradwords@gmail.com>
2018-06-25 11:42:32 +0530

    changed admin

--- onlinejobs/urls.py
+++ onlinejobs/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('ramk/', admin.site.urls),
     path('',include('basicsite.urls')),
     path('redirect/',include('redirect.urls'))
 ]
